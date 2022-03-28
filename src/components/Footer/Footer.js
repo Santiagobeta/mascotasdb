cba90c635b20c7b77634752726597fdc9b1a6769
Angela Alarcon <angela.alarcon@udea.edu.co>
2022-03-28 11:13:11 -0500

    Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <hr className="seperator" />
+                <section className='container flex justify-space-between align-center'>
+                    <div>
+                        <h3>Encuentra a tu peludito</h3>
+                        <p> Aplicación web donde se puede sistematizar los reportes y realizar consultas sobre las mascotas perdidas o en adopción en el Área Metropolitana, con el objetivo de que retornen pronto a su hogar o consigan uno, antes de llegar a convertirse en fauna feral, proporcionando datos sobre la mascota y la localización del lugar en que se extravió o reportó su avistamiento.  </p>
+                    </div>
+                    <div>
+                        <h3>Contáctanos</h3>
+                        <ul>angela.alarcon.bioing@gmail.com</ul>  
+                        <ul>bioing.santiago.betancur@gmail.com</ul> 
+                    </div>
+                </section>
+                <section className='container flex justify-space-between align-center'>
+                    <br></br>
+                    © 2022 Copyright
+                </section>
+                
+            <hr className="seperator" />
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/template/Template.js
+++ src/components/template/Template.js
@@ -1,12 +1,15 @@
 import "./Template.css";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
+
 
 function Template(props) {
   return (
     <>
       <Header></Header>
       <Outlet />
+      <Footer></Footer>
     </>
   );
 }
